fd09a9f4fb326d77eb3e2cd952775f40ec211f0c
T4ras123 <vovatara123@gmail.com>
2024-10-23 16:18:19 +0400

    feat(parquet): refactor parquet_to_tuple to parquet_to_tensor and add path checking

--- LaTeXTrOCR/dataset/parquet.py
+++ LaTeXTrOCR/dataset/parquet.py
@@ -3,13 +3,17 @@ import torch
 import os
 import PIL
 import imagesize
+import argparse
 import io
 import base64
 import pandas as pd
+from LaTeXTrOCR.utils.utils import path_exists
+from LaTeXTrOCR.tokenizer import Tokenizer
+
 
 ds = pd.DataFrame()
 
-def parquet_to_tuple(path=None) -> pd.DataFrame:
+def parquet_to_tensor(path=None) -> pd.DataFrame:
     """Convert parquet file to pandas dataframe 
     and convert bytes to a string of pixel values (H,W,C)
 
@@ -19,23 +23,17 @@ def parquet_to_tuple(path=None) -> pd.DataFrame:
     Returns:
         pd.DataFrame: str of pixel values in the "image" column and LaTex in the "text" one
     """
-    ds = pd.DataFrame(columns=['image', 'text'])
-    if path is not None:
-        dataset = pd.read_parquet(path)
-        for i in range(len(dataset.index)):
-            try:
-                base64_bytes = dataset['image'][i]["bytes"]
-                base64_string = base64_bytes.decode('latin1') 
-                missing_padding = len(base64_string) % 4
-                if missing_padding:
-                    base64_string += '=' * (4 - missing_padding)
-                
-                image_data = base64.b64decode(base64_string)
-                
-                ds = pd.concat([ds, pd.DataFrame({'image': [image_data], 'text': [dataset['text'][i]]})], ignore_index=True)
-            except (base64.binascii.Error, KeyError, UnicodeDecodeError) as e:
-                print(f"Error decoding base64 or missing key: {e}")
+    t = Tokenizer()
+    t.load_vocab("../model/dataset/tokenizer.json")
+    if path_exists(path):
+        df = pd.read_parquet(path)
+        print(df)
+    else:
+        pass
     
-    return ds
-
-ds1 = parquet_to_tuple('/home/vover/Vova/Github/UniNotes/data/external/LaTeXOCR-val-human_handwrite_print.parquet')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', type=str, default=None, help='Path to the parquet file.')
+    parser.parse_args()
+    
+    ds1 = parquet_to_tensor(parser.path)
